b8ab2140201115c0f8f5a27d85b173bf238345ed
Jrope21 <joshbball8@gmail.com>
2019-12-15 21:50:42 -0600

    back on da algorithm grind sonnn

---
+++ colt-steele-course/searching-algorithms/binary-search.js
@@ -0,0 +1,13 @@
+function binarySearch(arr, val) {
+    let left = 0;
+    let right = arr.length - 1;
+    let middle = Math.floor((left + right) / 2);
+    while (arr[middle] !== val && left <= right){
+        if(val < arr[middle]) right = middle - 1;
+        else left = middle + 1;
+        middle = Math.floor((left + right) / 2);
+    }
+    return arr[middle] === val ? middle : -1;
+}
+
+console.log(binarySearch([1,2,3,4,5,6,7,8,9,10, 12, 13], 11));
