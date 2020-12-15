cdfc662f0410bb7e791dffdc6a7e6438354b5604
jrope21 <joshbball8@gmail.com>
2020-12-15 00:05:14 -0600

    algo cardio

--- colt-steele-course/searching-algorithms/binary-search.js
+++ colt-steele-course/searching-algorithms/binary-search.js
@@ -1,13 +1,72 @@
-function binarySearch(arr, val) {
-    let left = 0;
-    let right = arr.length - 1;
-    let middle = Math.floor((left + right) / 2);
-    while (arr[middle] !== val && left <= right){
-        if(val < arr[middle]) right = middle - 1;
-        else left = middle + 1;
-        middle = Math.floor((left + right) / 2);
+// function binarySearch(arr, val) {
+//     let left = 0;
+//     let right = arr.length - 1;
+//     let middle = Math.floor((left + right) / 2);
+//     while (arr[middle] !== val && left <= right){
+//         if(val < arr[middle]) right = middle - 1;
+//         else left = middle + 1;
+//         middle = Math.floor((left + right) / 2);
+//     }
+//     return arr[middle] === val ? middle : -1;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const binarySearch = (sortedArr, value) => {
+    const left = 0;
+    const right = sortedArr.length - 1;
+    const middle;
+
+    while (left < right) {
+        middle = Math.floor((right + left) / 2);
+        if (value > middle) left = middle;
+        else right = middle;
     }
-    return arr[middle] === val ? middle : -1;
+    return sortedArr[middle] ? sortedArr[middle] : -1;
 }
+
 
 console.log(binarySearch([1,2,3,4,5,6,7,8,9,10, 12, 13], 11));
